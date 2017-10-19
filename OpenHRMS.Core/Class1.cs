407f5ec9225b2d0d5e211cbc9cf21d0463867abf
pganta <prasad.ganta@outlook.com>
2017-10-19 19:13:17 +0530

    initial commit

---
+++ OpenHRMS.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OpenHRMS.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ OpenHRMS.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OpenHRMS.Shared
+{
+    public class Class1
+    {
+    }
+}
